0e774299b6e8b8862ab2e97a99510be5213b4e33
Aakarsh Verma <ansh62050@gmail.com>
2026-08-08 21:41:24 +0530

    feat: complete product CRUD with get, update, delete

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -18,3 +18,46 @@ export const getProducts = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+    res.status(200).json({ success: true, data: product });
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+    res.status(200).json({ success: true, data: product });
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,13 @@
 import e from "express";
 import {
   createProduct,
+  deleteProduct,
+  getProduct,
   getProducts,
+  updateProduct,
 } from "../controllers/product.controller.js";
 const router = e.Router();
 
 router.route("/").post(createProduct).get(getProducts);
-
+router.route("/:id").patch(updateProduct).delete(deleteProduct).get(getProduct);
 export default router;
